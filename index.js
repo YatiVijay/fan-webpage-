fd3ae364d6883f985845cc174d2323c92e81a409
YatiVijay <81323019+YatiVijay@users.noreply.github.com>
2021-05-28 17:40:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const socketIO = require("socket.io");
+
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 5000, () =>
+  console.log("Server started!")
+);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat-message", (data) => {
+    io.sockets.emit("chat-message", data);
+  });
+});
